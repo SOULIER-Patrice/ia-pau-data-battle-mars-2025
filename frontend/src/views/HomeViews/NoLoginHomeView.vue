a24037489b5fd5ab85d5cce2a59791d9d735ded4
RolletQuentin <rolletquen@gmail.com>
2025-03-28 07:54:02 +0100

    final changes for deployment

--- frontend/src/views/HomeViews/NoLoginHomeView.vue
+++ frontend/src/views/HomeViews/NoLoginHomeView.vue
@@ -1,17 +1,6 @@
 <script setup lang="ts">
-// Stores
-import { useAuthStore } from '@/stores/authStore'
 import Logo from '@/assets/lawrag.png'
 
-import router from '@/router'
-
-const authStore = useAuthStore()
-const user = authStore.user
-
-// If the user is not logged in, redirect to the login page
-if (!user) {
-  router.push('/login')
-}
 </script>
 
 <template>
